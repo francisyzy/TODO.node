06593d18f1dfd3330eb11f77a79afe213771bbca
francisyzy <francisyzy@me.com>
2018-03-05 17:16:39 +0800

    Fix callback hell

--- index.js
+++ index.js
@@ -1,32 +1,33 @@
 const MongoClient = require('mongodb').MongoClient
-const assert = require('assert')
 
 const dboperation = require('./operations')
 const url = 'mongodb://localhost:27017/conFusion'
 
-MongoClient.connect(url, (err, db) => {
-  assert.equal(err, null)
+MongoClient.connect(url).then((db) => {
   console.log('connected successfully')
-  dboperation.insertDocument(db, {name: 'Test', description: 'asdasd'}, 'dishes', (result) => {
-    console.log('Insert document \n', result.ops)
-
-    dboperation.findDocument(db, 'dishes', (docs) => {
-      console.log('Found Documents \n', docs)
-
-      dboperation.updateDocument(db, {name: 'Test'}, {description: '123'}, 'dishes', (result) => {
-        console.log('Updated document \n', result.result)
-
-        dboperation.findDocument(db, 'dishes', (docs) => {
-          console.log('Found Updated Documents \n', docs)
-
-          const confusion = db.db('conFusion')
-          confusion.dropCollection('dishes', (result) => {
-            console.log('dropped collection')
-
-            db.close()
-          })
-        })
-      })
+  dboperation.insertDocument(db, {name: 'Test', description: 'asdasd'}, 'dishes')
+    .then((result) => {
+      console.log('Insert document \n', result.ops)
+      dboperation.findDocument(db, 'dishes')
     })
-  })
-})
+    .then((docs) => {
+      console.log('Found Documents \n', docs)
+      return dboperation.updateDocument(db, {name: 'Test'}, {description: '123'}, 'dishes')
+    })
+    .then((result) => {
+      console.log('Updated document \n', result.result)
+      return dboperation.findDocument(db, 'dishes')
+    })
+    .then((docs) => {
+      console.log('Found Updated Documents \n', docs)
+
+      const confusion = db.db('conFusion')
+      return confusion.dropCollection('dishes')
+    })
+    .then((result) => {
+      console.log('dropped collection')
+      return db.close()
+    })
+    .catch((err) => console.log(err))
+}, (err) => console.log(err))
+  .catch((err) => console.log(err))
