34b8925796dfa69d49e58a95592d3df54218d2a8
francisyzy <francisyzy@me.com>
2018-03-05 18:17:20 +0800

    Init Mongoose

--- index.js
+++ index.js
@@ -1,51 +1,35 @@
-const http = require('http')
-const fs = require('fs')
-const path = require('path')
+const mongoose = require('mongoose')
 
-const hostname = 'localhost'
-const port = 3000
+const Dishes = require('./models/dishes')
 
-// req = request
-// res = response
-const server = http.createServer((req, res) => {
-  console.log('URL: ' + req.url + ' Method: ' + req.method)
-
-  if (req.method === 'GET') {
-    var fileURL
-    if (req.url === '/') {
-      fileURL = '/index.html'
-      var filePath = path.resolve('./public' + fileURL)
-    } else {
-      fileURL = req.url
-      var filePath = path.resolve('./public' + fileURL + '.html')
-    }
-    const fileExt = path.extname(filePath)
-    if (fileExt === '.html') {
-      fs.exists(filePath, (exists) => {
-        if (!exists) {
-          res.statusCode = 404
-          res.setHeader('Content-Type', 'text/html')
-          res.end('<html><body><h1>Error 404: ' + fileURL + ' not found</h1><body><html>')
-
-          return
-        }
-
-        res.statusCode = 200
-        res.setHeader('Content-Type', 'text/html')
-        fs.createReadStream(filePath).pipe(res)
-      })
-    } else {
-      res.statusCode = 404
-      res.setHeader('Content-Type', 'text/html')
-      res.end('<html><body><h1>Error 404: ' + fileURL + ' not an html file</h1><body><html>')
-    }
-  } else {
-    res.statusCode = 404
-    res.setHeader('Content-Type', 'text/html')
-    res.end('<html><body><h1>Error 404: ' + req.method + ' not supported</h1><body><html>')
-  }
+const url = 'mongodb://localhost:27017/conFusion'
+const connect = mongoose.connect(url, {
+  useMongoClient: true
 })
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}`)
+connect.then((db) => {
+  console.log('Conencted to server')
+
+  var newDish = Dishes({
+    name: 'Dishasdname',
+    description: 'NASD'
+  })
+
+  newDish.save()
+    .then((dish) => {
+      console.log(dish)
+
+      return Dishes.find({}).exec()
+    })
+    .then((dishes) => {
+      console.log(dishes)
+
+      return db.collection('dishes').drop()
+    })
+    .then(() => {
+      console.log('dropped collection')
+
+      return db.close()
+    })
+    .catch((err) => console.log(err))
 })
