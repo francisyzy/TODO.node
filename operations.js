241301b553aac240fc419fd81414126cac8219da
francisyzy <francisyzy@me.com>
2018-03-05 15:56:49 +0800

    Add operations.js

---
+++ operations.js
@@ -0,0 +1,13 @@
+const assert = require('assert')
+
+exports.insertDocument = (db, document, collection, callback) => {
+
+}
+
+exports.findDocument = (db, collection, callback) => {
+
+}
+
+exports.removeDocument = (db, document, collection, callback) => {
+
+}
