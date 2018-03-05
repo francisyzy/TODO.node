07fcad0fbf01e396b7316f8b00d34b3527615788
francisyzy <francisyzy@me.com>
2018-03-05 21:37:23 +0800

    Add subcomment, update & push comment to item

--- index.js
+++ index.js
@@ -8,20 +8,32 @@ const connect = mongoose.connect(url)
 connect.then((db) => {
   console.log('Conencted to server')
 
-  var newDish = Dishes({
+  Dishes.create({
     name: 'Dishasdnaasdsdame',
     description: 'NASD'
   })
-
-  newDish.save()
     .then((dish) => {
       console.log(dish)
 
-      return Dishes.find({}).exec()
+      return Dishes.findByIdAndUpdate(dish._id, {
+        $set: {description: 'updated test'}
+      }, {
+        new: true
+      }).exec()
+    })
+    .then((dish) => {
+      console.log(dish)
+
+      dish.comments.push({
+        rating: 5,
+        comment: 'Very good fish',
+        author: 'John Smith'
+      })
+
+      return dish.save()
     })
     .then((dishes) => {
       console.log(dishes)
-
       // return db.collection('dishes').drop();
       return Dishes.remove() // https://stackoverflow.com/a/43943584
     })
