658f57bd6a2ed3a18583c377c9184dd9d2ab6046
francisyzy <francisyzy@me.com>
2018-03-08 14:56:42 +0800

    Added routing for tasks

--- routes/listsRoutes.js
+++ routes/listsRoutes.js
@@ -77,4 +77,139 @@ listRouter.route('/:listId')
       .catch((err) => next(err))
   })
 
+listRouter.route('/:listId/tasks')
+  .get((req, res, next) => {
+    Lists.findById(req.params.listId)
+      .then((list) => {
+        if (list != null) {
+          res.statusCode = 200
+          res.setHeader('Content-Type', 'application/json')
+          res.json(list.task)
+        } else {
+          var err = new Error('List ' + req.params.listId + ' not found')
+          err.statusCode = 404
+          return next(err)
+        }
+      }, (err) => next(err))
+      .catch((err) => next(err))
+  })
+  .post((req, res, next) => {
+    Lists.findById(req.params.listId)
+      .then((list) => {
+        if (list != null) {
+          list.tasks.push(req.body)
+          list.save()
+            .then((list) => {
+              res.statusCode = 200
+              res.setHeader('Content-Type', 'application/json')
+              res.json(list)
+            })
+        } else {
+          var err = new Error('List ' + req.params.listId + ' not found')
+          err.statusCode = 404
+          return next(err)
+        }
+      }, (err) => next(err))
+      .catch((err) => next(err))
+  })
+  .put((req, res, next) => {
+    res.statusCode = 403
+    res.end('Put operation not supported on /lists/' + req.params.listId)
+  })
+  .delete((req, res, next) => {
+    Lists.findById(req.params.listId)
+      .then((list) => {
+        if (list != null) {
+          for (var i = 0; i <= (list.task.length - 1); i++) {
+            list.task.id(list.task[i]._id).remove()
+          }
+          list.save()
+            .then((list) => {
+              res.statusCode = 200
+              res.setHeader('Content-Type', 'application/json')
+              res.json(list.task)
+            })
+        } else {
+          var err = new Error('List ' + req.params.listId + ' not found')
+          err.statusCode = 404
+          return next(err)
+        }
+      }, (err) => next(err))
+      .catch((err) => next(err))
+  })
+
+listRouter.route('/:listId/tasks/:taskId')
+  .get((req, res, next) => {
+    Lists.findById(req.params.listId)
+      .then((list) => {
+        if (list != null && list.task.id(req.params.taskId) != null) {
+          res.statusCode = 200
+          res.setHeader('Content-Type', 'application/json')
+          res.json(list.task.taskId)
+        } else {
+          var err = new Error('List ' + req.params.listId + ' not found')
+          err.statusCode = 404
+          return next(err)
+        }
+      }, (err) => next(err))
+      .catch((err) => next(err))
+  })
+  .post((req, res, next) => {
+    res.statusCode = 403
+    res.end('Post operation not supported on /lists/' + req.params.listId)
+  })
+  .put((req, res, next) => {
+    Lists.findById(req.params.listId)
+      .then((list) => {
+        if (list != null && list.task.id(req.params.taskId) != null) {
+          if (req.body.taskName) {
+            list.task.id(req.params.taskId).taskName = req.body.taskName
+          }
+          if (req.body.description) {
+            list.task.id(req.params.taskId).description = req.body.description
+          }
+          if (req.body.deadline) {
+            list.task.id(req.params.taskId).deadline = req.body.deadline
+          }
+          if (req.body.category) {
+            list.task.id(req.params.taskId).category = req.body.category
+          }
+          list.save()
+            .then((list) => {
+              res.statusCode = 200
+              res.setHeader('Content-Type', 'application/json')
+              res.json(list.task.taskId)
+            })
+        } else {
+          var err = new Error('List ' + req.params.listId + ' not found')
+          err.statusCode = 404
+          return next(err)
+        }
+      }, (err) => next(err))
+      .catch((err) => next(err))
+  })
+  .delete((req, res, next) => {
+    Lists.findById(req.params.listId)
+      .then((list) => {
+        if (list != null && list.task.id(req.params.taskId) != null) {
+          list.task.id(list.task.taskId).remove()
+          list.save()
+            .then((list) => {
+              res.statusCode = 200
+              res.setHeader('Content-Type', 'application/json')
+              res.json(list.task)
+            })
+        } else if (list == null) {
+          var err = new Error('List ' + req.params.listId + ' not found')
+          err.statusCode = 404
+          return next(err)
+        } else {
+          err = new Error('Task ' + req.params.listId + ' not found')
+          err.statusCode = 404
+          return next(err)
+        }
+      }, (err) => next(err))
+      .catch((err) => next(err))
+  })
+
 module.exports = listRouter
