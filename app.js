8aafaa5e67fcbc4023ea15704a63f6b7e63b39e0
francisyzy <francisyzy@me.com>
2018-03-27 13:47:38 +0800

    Change to react view engine

--- app.js
+++ app.js
@@ -37,7 +37,9 @@ connect.then((db) => {
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'jade')
+// app.set('view engine', 'jade')
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
 
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
 app.use(logger('dev'))
